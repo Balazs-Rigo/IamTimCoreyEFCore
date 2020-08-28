4a1b89850b4d615d5ef8f0a335c3c32c15bb76b0
Balázs Rigó <rigo.balazs86@gmail.com>
2020-08-28 17:49:45 +0200

    Finished!

--- EFSolution/EFConsoleUI/Program.cs
+++ EFSolution/EFConsoleUI/Program.cs
@@ -1,5 +1,8 @@
 ﻿using EFConsoleUI.DataAccess;
+using EFConsoleUI.Models;
 using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace EFConsoleUI
 {
@@ -7,8 +10,70 @@ namespace EFConsoleUI
     {
         static void Main(string[] args)
         {
-        
+            //CreateTim();
+            //ReadAll();
+            UpdateFirstName(1,"Macko");
+            ReadById(1);
+
+            Console.WriteLine("Done Processing!");
             Console.ReadLine();
+        }
+
+        private static void CreateTim()
+        {
+            var c = new Contact
+            {
+                FirstName = "Tim",
+                LastName = "Corey"
+            };
+            c.EmailAddresses.Add(new Email {EmailAddresses="tim@iamtimcorey.com" });
+            c.EmailAddresses.Add(new Email { EmailAddresses="me@timcorey.com"});
+            c.PhoneNumbers.Add(new Phone { PhoneNumber = "555-1212" });
+            c.PhoneNumbers.Add(new Phone { PhoneNumber = "555-1234" });
+
+            using (var db =  new ContactContext())
+            {
+                db.Contacts.Add(c);
+                db.SaveChanges();
+            }
+        }
+
+        private static void ReadAll()
+        {
+            using (var db = new ContactContext())
+            {
+                var records = db.Contacts
+                    .Include(e => e.EmailAddresses)
+                    .Include(p=>p.PhoneNumbers)
+                    .ToList();
+
+                foreach (var c in records)
+                {
+                    Console.WriteLine($"{c.FirstName} {c.LastName}");
+                }
+            }
+        }
+
+        private static void ReadById(int id)
+        {
+            using (var db = new ContactContext())
+            {
+                var user = db.Contacts.Where(c=>c.Id ==id).First();
+
+                Console.WriteLine($"{user.FirstName} {user.LastName}");
+            }
+        }
+
+        private static void UpdateFirstName(int id, string firstName)
+        {
+            using (var db = new ContactContext())
+            {
+                var user = db.Contacts.Where(c => c.Id == id).First();
+
+                user.FirstName = firstName;
+
+                db.SaveChanges();
+            }
         }
     }
 }
